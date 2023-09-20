43978b2da6bae989e2d20a538fb9392c673c92b2
Manish10022001 <145294362+Manish10022001@users.noreply.github.com>
2023-09-20 12:46:38 +0530

    Add files via upload

---
+++ day5/ms/country.java
@@ -0,0 +1,16 @@
+//To demostrate multilevel inheritance
+package com.day5.ms;
+
+public class country {
+
+	private String countryname;
+	private String capital;
+	
+	public  String getCountryname() {
+			return countryname;
+
+	}
+	
+	
+
+}
