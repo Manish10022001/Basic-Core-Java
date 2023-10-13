ed2a9eb9d7324864e38551b31c88bddf3c720b93
Manish10022001 <145294362+Manish10022001@users.noreply.github.com>
2023-10-13 00:13:11 -0700

    first commit

---
+++ daynine/WrappperClassDemo.java
@@ -0,0 +1,25 @@
+package com.daynine;
+
+public class WrappperClassDemo {
+
+	public static void main(String[] args) {
+		// unboxing
+		
+		Integer i = new Integer (10);
+		System.out.println(i);
+		
+		int b = i.intValue();
+		System.out.println(b);
+		
+		//without using intValue()
+		int c = i;
+		System.out.println(c);
+		
+		//Autoboxing
+		int a = 100;
+		Integer i1 = a;
+		System.out.println(i1);
+		
+	}
+
+}
